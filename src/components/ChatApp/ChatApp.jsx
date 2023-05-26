4238f4d95022ec7f45c7eaeb962f66d80426b40c
hamongkhang <hamongkhang@gmail.com>
2023-05-26 19:28:48 +0700

    feat: create chat app component

---
+++ src/components/ChatApp/ChatApp.jsx
@@ -0,0 +1,19 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Grid } from "@mui/material";
+import Menu from "../Menu/Menu";
+import Content from "../Content/Content";
+
+function ChatApp() {
+  return (
+    <Grid container columns={{ xs: 4, sm: 8, md: 12 }}>
+      <Grid item xs={4} sm={2} md={3}>
+        <Menu />
+      </Grid>
+      <Grid item xs={4} sm={6} md={9}>
+        <Content />
+      </Grid>
+    </Grid>
+  );
+}
+
+export default ChatApp;
